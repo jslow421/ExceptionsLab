94b01e51f00549cd17545a62862a9aad46e56fcf
jslowik <jslowik@10.30.19.181>
2015-10-21 17:40:17 -0500

    in-class example

--- src/lab1/Challenge1.java
+++ src/lab1/Challenge1.java
@@ -21,55 +21,64 @@ public class Challenge1 {
 
     public static void main(String[] args) {
         Challenge1 app = new Challenge1();
-        
-        
-        String fullName = JOptionPane.showInputDialog("Enter full name (use Format: first last):");
-        
+        boolean askAgain = false;
         String lastName = null;
-        try{
-            lastName = app.extractLastName(fullName);
-        }catch(IllegalArgumentException iae){
-            System.out.println(iae.getMessage());
-        }
-        String msg = "Your last name is: " + lastName;
+        String fullName = null;
+        do {
+            fullName = JOptionPane.showInputDialog("Enter full name (use Format: first last):");
+            try {
+                lastName = app.extractLastName(fullName);
+                askAgain = false;
+            } catch (IllegalArgumentException iae) {
+                JOptionPane.showMessageDialog(null, iae.getMessage());
+                askAgain = true;
+            }
+        } while (askAgain);
+
+        
+
+        String msg = "Your last name appears to be (assumes the last word "
+                + "is your last name): " + lastName;
         JOptionPane.showMessageDialog(null, msg);
+
+        /*poor attempt -- should be in the method and output here
+         String fullName = JOptionPane.showInputDialog("Enter full name (use Format: first last):");
         
-/*poor attempt -- should be in the method and output here
-        String fullName = JOptionPane.showInputDialog("Enter full name (use Format: first last):");
-        
-            JOptionPane.showMessageDialog(null, "Please enter a name");
-            if(fullName == null){
-                JOptionPane.showMessageDialog(null, "You must enter a name");
-            }
-            else {
-            String lastName = app.extractLastName(fullName);
-            if(fullName.equals(lastName)){
-                JOptionPane.showMessageDialog(null,"Enter both a first and last name");
-            }else{
-            String msg = "Your last name is: " + lastName;
-            JOptionPane.showMessageDialog(null, msg);
-            }
-        } */
+         JOptionPane.showMessageDialog(null, "Please enter a name");
+         if(fullName == null){
+         JOptionPane.showMessageDialog(null, "You must enter a name");
+         }
+         else {
+         String lastName = app.extractLastName(fullName);
+         if(fullName.equals(lastName)){
+         JOptionPane.showMessageDialog(null,"Enter both a first and last name");
+         }else{
+         String msg = "Your last name is: " + lastName;
+         JOptionPane.showMessageDialog(null, msg);
+         }
+         } */
     }
+
     /**
-     * Attempts to extract last name from full name constructed of two
-     * parts -- first name and last name. This method cannot guarantee that the last
-     * part is the last name. It must be assumed so. Be careful.
+     * Attempts to extract last name from full name constructed of two parts --
+     * first name and last name. This method cannot guarantee that the last part
+     * is the last name. It must be assumed so. Be careful.
+     *
      * @param fullName a multi-part entry representing a full name
-     * @return 
-     * @throws IllegalArgumentException if full name is null, or full name is empty,
-     * or full name has less than two parts. If full name has more than two parts
-     * no exception will be thrown, because we cannot know if the last part is
-     * definitely a last name, so we will assume that it is.
+     * @return
+     * @throws IllegalArgumentException if full name is null, or full name is
+     * empty, or full name has less than two parts. If full name has more than
+     * two parts no exception will be thrown, because we cannot know if the last
+     * part is definitely a last name, so we will assume that it is.
      */
 
-    public String extractLastName(String fullName) throws IllegalArgumentException{
-        if(fullName == null || fullName.isEmpty()){
+    public String extractLastName(String fullName) throws IllegalArgumentException {
+        if (fullName == null || fullName.isEmpty()) {
             throw new IllegalArgumentException("Sorry, full name is required");
         }
 
         String[] nameParts = fullName.split(" ");
-        if(nameParts.length < 2){
+        if (nameParts.length < 2) {
             throw new IllegalArgumentException("Sorry, you must provide both a "
                     + "first name and a last name");
         }
